49cd0ff4504bd4b37b0c1c3eb42df3424a12217a
danielh59 <dev.hoyte@gmail.com>
2023-03-11 05:22:22 -0500

    jss css files

---
+++ js/styling.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    document.getElementById("jump"){
+        let counter = 0;
+        jump.onclick = function (){
+            counter += 1
+            console.log(counter);
+
+        }
+            
+    }
+}
